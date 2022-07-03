03ef4b282fe5bf19eb8da534bace8dfa46e0aeef
Alejo Rios <alejoriosiglesias@gmail.com>
2022-07-03 13:16:41 +0100

    feat: filtramos assets que empiezan con EUR

--- llamada.py
+++ llamada.py
@@ -1,12 +1,12 @@
 import requests
-
+# subir la api_key al repo si es publico, es una mala practica ya que cualquiera podria usarla
 api_key = "D3B23BBC-48BC-477B-B842-1BC9796DB7F4"
 headers = {
     'X-CoinAPI-Key' : api_key
 }
-api_url = "http://rest-sandbox.coinapi.io"
+api_url = "http://rest.coinapi.io"
 # el endpoint o ruta lo separamos de la URL para consultar varias rutas y no tener que cambiar todo
-endpoint = "/v1/exchanges"
+endpoint = "/v1/assets"
 
 url = api_url + endpoint
 
@@ -15,8 +15,13 @@ response = requests.get(url, headers=headers)
 code = response.status_code
 
 if code == 200:
-    print("El resultado de la consulta es: ")
-    print(response.text)
+    print("Las monedas disponibles son: ")
+    # convertimos a json para interpretarlo en python como un diccionario
+    response_json = response.json()
+    
+    for coin in response_json:
+        if coin ["asset_id"].startswith("EUR"):    
+            print(coin["asset_id"], coin["name"])
 else:
     print("La peticion a la API ha fallado")
     print(f"Codigo del error {code}")
